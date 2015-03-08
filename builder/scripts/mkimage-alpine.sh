559d998bc933c0ccd87bb72473043886ef965873
Andy Shinn <andys@andyshinn.as>
2015-03-07 22:13:15 -0600

    wip: refactoring for build options per repo

--- builder/scripts/mkimage-alpine.sh
+++ builder/scripts/mkimage-alpine.sh
@@ -6,9 +6,9 @@
 # Luis Lavena (luislavena).
 
 set -e
-[ $TRACE ] && set -x
+[ "$TRACE" ] && set -x
 
-[ $(id -u) -eq 0 ] || {
+[ "$(id -u)" -eq 0 ] || {
 	printf >&2 '%s requires root\n' "$0"
 	exit 1
 }
@@ -19,65 +19,66 @@ usage() {
 }
 
 tmp() {
-	TMP=$(mktemp -d ${TMPDIR:-/var/tmp}/alpine-docker-XXXXXXXXXX)
-	ROOTFS=$(mktemp -d ${TMPDIR:-/var/tmp}/alpine-docker-rootfs-XXXXXXXXXX)
-	trap "rm -rf $TMP $ROOTFS" EXIT TERM INT
+	TMP="$(mktemp -d "${TMPDIR:-/var/tmp}/alpine-docker-XXXXXXXXXX")"
+	ROOTFS="$(mktemp -d "${TMPDIR:-/var/tmp}/alpine-docker-rootfs-XXXXXXXXXX")"
+	trap 'rm -rf $TMP $ROOTFS' EXIT TERM INT
 }
 
 apkv() {
-	curl -sSL $REPO/$ARCH/APKINDEX.tar.gz | tar -Oxz |
+	curl -sSL "${REPO}/${ARCH}/APKINDEX.tar.gz" | tar -Oxz |
 		grep '^P:apk-tools-static$' -a -A1 | tail -n1 | cut -d: -f2
 }
 
 getapk() {
-	curl -sSL $REPO/$ARCH/apk-tools-static-$(apkv).apk |
-		tar -xz -C $TMP sbin/apk.static
+	local version="$(apkv)"
+	curl -sSL "${REPO}/${ARCH}/apk-tools-static-${version}.apk" |
+		tar -xz -C "$TMP" sbin/apk.static
 }
 
 mkbase() {
-	$TMP/sbin/apk.static --repository $REPO --update-cache --allow-untrusted \
-		--root $ROOTFS --initdb add alpine-base
+	"${TMP}/sbin/apk.static" --repository "$REPO" --update-cache --allow-untrusted \
+		--root "$ROOTFS" --initdb add alpine-base
 }
 
 timezone() {
 	local timezone="${1:-UTC}"
 
-	$TMP/sbin/apk.static \
-		--repository $REPO \
+	"${TMP}/sbin/apk.static" \
+		--repository "$REPO" \
 		--update-cache \
 		--allow-untrusted \
-		--root $ROOTFS \
+		--root "$ROOTFS" \
 		--initdb add tzdata
 	cp -a "${ROOTFS}/usr/share/zoneinfo/${timezone}" "${ROOTFS}/etc/localtime"
-	$TMP/sbin/apk.static \
-		--root $ROOTFS \
+	"${TMP}/sbin/apk.static" \
+		--root "$ROOTFS" \
 		del tzdata
 }
 
 conf() {
-	printf '%s\n' $REPO > $ROOTFS/etc/apk/repositories
-	[ $REPO_EXTRA -eq 1 ] && {
-		[ $REL = "edge" ] || printf '%s\n' "@edge $MIRROR/edge/main" >> $ROOTFS/etc/apk/repositories
-		printf '%s\n' "@testing $MIRROR/edge/testing" >> $ROOTFS/etc/apk/repositories
+	printf '%s\n' "$REPO" > "${ROOTFS}/etc/apk/repositories"
+	[ "$REPO_EXTRA" -eq 1 ] && {
+		[ "$REL" = "edge" ] || printf '%s\n' "@edge $MIRROR/edge/main" >> "${ROOTFS}/etc/apk/repositories"
+		printf '%s\n' "@testing $MIRROR/edge/testing" >> "${ROOTFS}/etc/apk/repositories"
 	}
-	[ $ADD_APK_SCRIPT -eq 1 ] && cp /apk-install $ROOTFS/usr/sbin/apk-install
-	[ $ADD_TIMEZONE -eq 1 ] && timezone
-	rm -f $ROOTFS/var/cache/apk/*
+	[ "$ADD_APK_SCRIPT" -eq 1 ] && cp /apk-install "${ROOTFS}/usr/sbin/apk-install"
+	timezone "UTC"
+	rm -f "$ROOTFS"/var/cache/apk/*
 }
 
 save() {
-	[ $SAVE -eq 1 ] || return
+	[ "$SAVE" -eq 1 ] || return
 
-	tar --numeric-owner -C $ROOTFS -c . | xz > rootfs.tar.xz
+	tar -z -f rootfs.tar.gz --numeric-owner -C "$ROOTFS" -c .
 }
 
-while getopts "hr:m:sect" opt; do
+while getopts "hr:m:t:sec" opt; do
 	case $opt in
 		r)
-			REL=$OPTARG
+			REL="$OPTARG"
 			;;
 		m)
-			MIRROR=$OPTARG
+			MIRROR="$OPTARG"
 			;;
 		s)
 			SAVE=1
@@ -86,7 +87,7 @@ while getopts "hr:m:sect" opt; do
 			REPO_EXTRA=1
 			;;
 		t)
-			ADD_TIMEZONE=1
+			TIMEZONE="$OPTARG"
 			;;
 		c)
 			ADD_APK_SCRIPT=1
@@ -97,13 +98,13 @@ while getopts "hr:m:sect" opt; do
 	esac
 done
 
-REL=${REL:-edge}
-MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
-SAVE=${SAVE:-0}
-ADD_APK_SCRIPT=${ADD_APK_SCRIPT:-0}
-ADD_TIMEZONE=${ADD_TIMEZONE:-0}
-REPO=$MIRROR/$REL/main
-REPO_EXTRA=${REPO_EXTRA:-0}
-ARCH=$(uname -m)
+REL="${REL:-edge}"
+MIRROR="${MIRROR:-http://nl.alpinelinux.org/alpine}"
+SAVE="${SAVE:-0}"
+ADD_APK_SCRIPT="${ADD_APK_SCRIPT:-0}"
+TIMEZONE="${TIMEZONE:-UTC}"
+REPO="$MIRROR/$REL/main"
+REPO_EXTRA="${REPO_EXTRA:-0}"
+ARCH="$(uname -m)"
 
-tmp && getapk && mkbase && conf && save
+tmp 1>&2 && getapk 1>&2 && mkbase 1>&2 && conf 1>&2 && save
